86b135fec114b57f4993f7d7a838d297d19b9f03
otaboy <rakhmonovotaboy0586@gmail.com>
2025-03-07 17:57:47 +0500

    full finish +1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,12 @@
-// import createMiddleware from "next-intl/middleware";
-// import { locales } from "../i18n.configs"
-
-
-// export default createMiddleware({
-//     locales,
-//     defaultLocale: 'en'
-// })
-
-
-// export const config = {
-//     matcher : ["/", "/(en|uz|ru)/:path*"],
-// };
- 
-
 
 import createMiddleware from "next-intl/middleware";
 import { locales, Locale } from "../i18n.configs";
 
 export default createMiddleware({
-    locales: locales as readonly string[], // TypeScript uchun aniq tip
-    defaultLocale: "en" as const, // defaultLocale ham aniq qiymat
+    locales: locales as readonly string[],
+    defaultLocale: "en" as const,
 });
 
 export const config = {
-    matcher: ["/", "/(en|uz|ru)/:path*"], // Hozircha OK, lekin dinamik qilish mumkin
+    matcher: ["/", "/(en|uz|ru)/:path*"], 
 };
