9c27e9c2b9270610ac5eed13fbbc861eef53c8d4
otaboy <rakhmonovotaboy0586@gmail.com>
2025-03-11 12:49:02 +0500

    contact_us button bosilganda togri positsiyada joylashdi

--- src/components/button/contact-us/contact-us.tsx
+++ src/components/button/contact-us/contact-us.tsx
@@ -3,16 +3,24 @@ import "./contact-us.scss";
 
 const ContactUs = () => {
   const t = useTranslations("ContactUs");
-  const handleContactClick = () => {
-    const footerElement = document.getElementById("request");
-    if (footerElement) {
-      footerElement.scrollIntoView({ behavior: "smooth" }); // Smooth scroll qilish
+
+  const scrollToSection = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+        const navbarHeight = document.querySelector(".fixed")?.getBoundingClientRect().height || 0;
+        const extraOffset = 10;
+        const elementPosition = element.getBoundingClientRect().top + window.pageYOffset - navbarHeight - extraOffset;
+
+        window.scrollTo({
+            top: elementPosition,
+            behavior: "smooth",
+        });
     }
-  };
+};
 
   return (
     <div className="contact-us">
-      <button className="" onClick={handleContactClick}> {t ("button")} </button>
+      <button className="" onClick={()=> scrollToSection("request")}> {t ("button")} </button>
     </div>
   );
 };
