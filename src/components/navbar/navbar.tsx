e2ae32797840a0d00db33787cecd0b221da5940a
otaboy <rakhmonovotaboy0586@gmail.com>
2025-03-12 17:17:35 +0500

    Main Logo fixed code

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -69,7 +69,7 @@ const Navbar = ({ locale }: { locale: Locale }) => {
           <div className="main-navbar_header">
             <div className="main-logo">
               <Link href="/" onClick={handleLinkClick}>
-                <Image className="main-logo_img logo" src="/svgs/logo.svg" alt="Logo" width={130} height={100} style={{ objectFit: 'contain' }} />
+                <Image className="main-logo_img logo" src="/imgs/logo1.jpg" alt="Logo" width={130} height={100} style={{ objectFit: 'contain' }} />
               </Link>
             </div>
 
