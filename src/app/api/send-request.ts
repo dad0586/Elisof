66cebd1e7e485030fc6b7fdefa26eaf0fa5b399b
otaboy <rakhmonovotaboy0586@gmail.com>
2025-03-13 16:53:48 +0500

    fix code

--- src/app/api/send-request.ts
+++ src/app/api/send-request.ts
@@ -6,13 +6,10 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     const { firstName, lastName, email, phone, request } = req.body;
-    // Bu yerda ma'lumotlarni qayta ishlash (masalan, bazaga saqlash yoki log qilish)
     console.log("Received data:", { firstName, lastName, email, phone, request });
 
-    // Muvaffaqiyatli javob qaytarish
     res.status(200).json({ message: "Request received successfully" });
   } else {
-    // Faqat POST so'rovlariga ruxsat beriladi
     res.status(405).json({ message: "Method not allowed" });
   }
 }
